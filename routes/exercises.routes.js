1cd1db3a551cf66f6f7a9a5cd10f0d08ab5a7287
Mikko Peltonen <mikko.inno.peltonen@gmail.com>
2024-11-02 11:54:44 +0200

    updated code

--- routes/exercises.routes.js
+++ routes/exercises.routes.js
@@ -13,7 +13,7 @@ router.post('/by-muscle-groups', async (req, res) => {
         console.log(muscleGroups)
         const muscleGroupIds = muscleGroups.map(muscleGroup => muscleGroup._id)
         console.log(muscleGroupIds)
-        const exercises = await Exercise.find({ belongsTo: { $in: muscleGroupIds } }).populate('belongsTo')
+        const exercises = await Exercise.find({ belongsTo: { $in: muscleGroupIds } }).populate('belongsTo').limit(5)
         console.log(exercises)
         res.json(exercises)
     } catch (error) {
