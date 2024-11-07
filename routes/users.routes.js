70f1d1111099446ac753e0b39aa4833a63897396
Mikko Peltonen <mikko.inno.peltonen@gmail.com>
2024-11-07 18:59:40 +0200

    building up user fetching api

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -8,8 +8,11 @@ const mongoose = require('mongoose')
 router.get('/', async (req, res) => {
 
     try {
+        console.log('In user route')
         const userId = req.payload._id
+        console.log('user Id: ', userId)
         const userData = await User.findById(mongoose.Types.ObjectId(userId))
+        console.log('found user: ', userData)
         res.status(200).json(userData.name)
     } catch (error) {
         console.error(error)
